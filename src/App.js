c1b6e1713828240a01df79d57583a1fcb62a5c76
jonson <luizlindonjonson@yahoo.com.br>
2020-07-25 00:23:10 -0300

    consumindo API com axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
+import axios from 'axios'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
   const [usuario, setUsuario] = useState('');
+  function handlePesquisa() {
+    // console.log(usuario)
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+    .then(response=> console.log(response.data))
+
+  }
   return (
     <>
 
-      <h1> {usuario}</h1>
-      <input placeholder="Usuário" className="usuarioInput"value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <button type="button">Pesquisa</button>
+      <h1> Repositorios do GitHub {usuario} </h1>
+      
+        <input placeholder="Usuário" className="usuarioInput" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <button type="button" onClick={handlePesquisa}>Pesquisa</button>
+      
     </>
   );
 }
